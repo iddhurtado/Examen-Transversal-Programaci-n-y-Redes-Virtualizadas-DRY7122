b5f99cb83663d8f910c214abc874514cfdaed0cb
SampleUser <name@sampleuser.com>
2025-07-11 03:02:37 +0000

    Agrego scripts de integrantes y verificación de VLAN para el examen DRY7122

---
+++ integrantes.py
@@ -0,0 +1,7 @@
+# integrantes.py
+
+integrantes = ["Israel Hurtado", "Francisca Álvarez", "Diana Burboa"]
+
+print("Integrantes del grupo:")
+for nombre in integrantes:
+    print("-", nombre)
---
+++ vlan.py
@@ -0,0 +1,14 @@
+# vlan_checker.py
+
+try:
+    vlan = int(input("Ingrese el número de VLAN: "))
+
+    if 1 <= vlan <= 1005:
+        print("VLAN de rango normal.")
+    elif 1006 <= vlan <= 4094:
+        print("VLAN de rango extendido.")
+    else:
+        print("Número de VLAN no válido (fuera de rango).")
+
+except ValueError:
+    print("Por favor ingrese un número válido.")
